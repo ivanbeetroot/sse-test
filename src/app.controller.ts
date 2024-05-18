ac384f29826888ddb6a55c469e7c541ddda431fb
Ivan Litovchenko <ivan.litovchenko@beetroot.com.ua>
2024-05-18 16:56:02 +0300

    sse

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { BadRequestException, Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,28 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('sse')
+  sse(): Observable<any> {
+    return new Observable((subscriber) => {
+      Promise.resolve()
+        .then(async () => {
+          subscriber.next({ data: 'Hello World!' });
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+          throw new BadRequestException('Error');
+          // subscriber.next({ data: 'Hello World!1' });
+          // subscriber.complete();
+        })
+        .catch((error) => {
+          subscriber.next({
+            data: {
+              error: error.message,
+              code: error.getStatus(),
+            },
+          });
+          subscriber.error(error);
+        });
+    });
   }
 }
